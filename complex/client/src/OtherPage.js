5b68e9baca1950ce66414f8c9da23c5a67f3c49c
arindams <s.arindam83@gmail.com>
2020-07-18 13:37:00 +0530

    Updated react app and added new docker files

--- complex/client/src/OtherPage.js
+++ complex/client/src/OtherPage.js
@@ -2,13 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default () => {
-
   return (
     <div>
-     Im some other page!
-     <Link to="/"> Go Back Home</Link>
+      Im some other page
+      <Link to="/">Go back to home page!</Link>
     </div>
   );
-
-});
-
+};
